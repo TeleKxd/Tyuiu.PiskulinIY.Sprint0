de1513f160f0cda107584e6f61903f6b6b4ec1ca
TeleKxd <piskulin.i@mail.ru>
2024-09-18 12:46:27 +0300

    Task2

---
+++ Tyuiu.PiskulinIY.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PiskulinIY.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PiskulinIY.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3cab0b51-c312-4f12-ade0-0add0f535ad8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PiskulinIY.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PiskulinIY.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.PiskulinIY.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызов класса DataService и метода GetMessega
+            //из библиотеки Tyuiu.PiskulinIY.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Игорь"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}

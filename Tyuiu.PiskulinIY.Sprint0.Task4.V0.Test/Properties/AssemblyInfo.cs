a93addf2bee1a3aef107fef54e30a2b197f2763f
TeleKxd <piskulin.i@mail.ru>
2024-09-18 17:35:41 +0300

    Task4

---
+++ Tyuiu.PiskulinIY.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PiskulinIY.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PiskulinIY.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08c329be-8843-4489-b8ef-528662a50b15")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PiskulinIY.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.PiskulinIY.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.PiskulinIY.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример линейной структуры программы
+            //Вызов метода сложения Addition
+            Console.WriteLine(DataService.Addition(1, 5));
+
+            //Вызов метода сложения Subtracktion
+            Console.WriteLine(DataService.Subtraction(15, 5));
+
+            //Вызов метода сложения Multiplication 
+            Console.WriteLine(DataService.Multiplication(10, 10));
+
+            //Вызов метода сложения Division
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}

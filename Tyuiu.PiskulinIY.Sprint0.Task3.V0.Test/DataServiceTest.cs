84211b67e5d1c2699af045fae12c3347f72aef0e
TeleKxd <piskulin.i@mail.ru>
2024-09-18 16:00:24 +0300

    Task3

---
+++ Tyuiu.PiskulinIY.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PiskulinIY.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.PiskulinIY.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual( 10, DataService.Sum( 5,5) );
+        }
+    }
+}
---
+++ Tyuiu.PiskulinIY.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PiskulinIY.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PiskulinIY.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fdef0685-978d-4c98-9183-2bfcf235650c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
